bc488c0e68d5314856e847c87f2428f30fff3dc7
Eros000 <e.ghilardi@hotmail.it>
2023-06-16 08:06:49 +0200

    commit

---
+++ js/script.js
@@ -0,0 +1,35 @@
+
+
+
+const emailList = [
+    
+    'pippo@gmail.com', 
+    'pluto@gmail.com', 
+    'paperino@gmail.com', 
+    'minni@gmail.com', 
+    'topolino@gmail.com', 
+    'tony@gmail.com', 
+    'bruce@gmail.com', 
+]
+
+
+const myEmail = document.getElementById('my-email').value;
+
+if (myEmail === emailList){
+    
+}
+
+
+
+
+const form = document.querySelector('form');
+
+form.addEventListener ('click',
+
+    function (event){
+        event.preventDefault();
+        
+    }
+
+)
+
